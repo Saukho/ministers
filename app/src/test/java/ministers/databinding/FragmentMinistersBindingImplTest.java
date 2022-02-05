3380bf0958d7aaed5f5efe6bbaf248d3f3f9559e
sami sihvonen <sami.sihvonen2@metropolia.fi>
2022-02-05 12:35:16 +0200

    navigation fragment to ministerDetailsFragment working

---
+++ app/src/test/java/ministers/databinding/FragmentMinistersBindingImplTest.java
@@ -0,0 +1,24 @@
+package ministers.databinding;
+
+import junit.framework.TestCase;
+
+public class FragmentMinistersBindingImplTest extends TestCase {
+
+    public void testInvalidateAll() {
+    }
+
+    public void testHasPendingBindings() {
+    }
+
+    public void testSetVariable() {
+    }
+
+    public void testSetMinistersFragment() {
+    }
+
+    public void testOnFieldChange() {
+    }
+
+    public void testExecuteBindings() {
+    }
+}
